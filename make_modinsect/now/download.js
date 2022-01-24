0dbaade101c82dbd831004cbd39fe86b1a35502d
souta-1326 <62239382+souta-1326@users.noreply.github.com>
2022-01-24 20:59:37 +0900

    Delete make_modinsect/now directory

--- make_modinsect/now/download.js
+++
@@ -1,7 +0,0 @@
-function download(){
-  const canvas = document.getElementById('board');
-  const modinsect_link = document.createElement("a");
-  modinsect_link.href = canvas.toDataURL();
-  modinsect_link.download = "modinsect.png";
-  modinsect_link.click();
-}
